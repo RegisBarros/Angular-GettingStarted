1b9570b4fffd12aee200179ee5cfdf022601afe3
Reginaldo Alves de Barros <R21Regis@hotmail.com>
2018-02-11 18:09:09 -0200

    advanced route

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -8,10 +10,27 @@ import { IProduct } from './product';
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
+  errorMessage: string;
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute, private _router: Router,
+    private _productService: ProductService) { }
 
   ngOnInit() {
+    const param = this._route.snapshot.paramMap.get('id');
+    if(param) {
+      const id = +param;
+      this.getProduct(id);
+    }
   }
 
+  getProduct(id: number) {
+    this._productService.getProduct(id)
+    .subscribe(product =>
+      this.product = product,
+      error => this.errorMessage = <any>error);
+  }
+
+  onBack(): void {
+    this._router.navigate(['/products']);
+  }
 }
